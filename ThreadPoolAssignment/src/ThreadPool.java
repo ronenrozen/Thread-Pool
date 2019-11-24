3877b9cb00da563ff16b7cb87b241e3b865d176b
slava <slava15.92@gmail.com>
2019-11-24 17:44:44 +0200

    Fixed bugs and main updated

--- ThreadPoolAssignment/src/ThreadPool.java
+++ ThreadPoolAssignment/src/ThreadPool.java
@@ -1,12 +1,12 @@
 import java.util.concurrent.Callable;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class ThreadPool {
+class ThreadPool {
     private final int numberOfThreads;
     private final LinkedBlockingQueue tasksList;
     private final Worker[] threads;
 
-    public ThreadPool(int numberOfThreads) {
+    ThreadPool(int numberOfThreads) {
         this.numberOfThreads = numberOfThreads;
         tasksList = new LinkedBlockingQueue();
         threads = new Worker[numberOfThreads];
@@ -17,23 +17,26 @@ public class ThreadPool {
         }
     }
 
-    public double[] run(Callable task) {
+    double[] run(Callable task) {
         synchronized (tasksList) {
             tasksList.add(task);
             tasksList.notify();
         }
-        for (Worker<double[]> w : threads) {
-            try {
-                synchronized (w) {
-                    if (w.getCurrentTask() != null && w.getCurrentTask().equals(task)) {
-                        w.notify();
-                        return w.getResults();
+        while (true) {
+            for (Worker<double[]> w : threads) {
+                try {
+                    synchronized (w) {
+                        if (w.getCurrentTask() != null && w.getCurrentTask().equals(task)) {
+                            w.notify();
+                            if (w.getResults() != null) {
+                                return w.getResults();
+                            }
+                        }
                     }
+                } catch (Exception e) {
+                    //nothing
                 }
-            } catch (Exception e) {
-                //nothing
             }
         }
-        return null;
     }
 }
